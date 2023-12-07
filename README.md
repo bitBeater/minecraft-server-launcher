16a2d01bf9bbe53fb48fea58e27cdd34a400e9cb
alexrr2iggs <alexandru.bunduche@yahoo.com>
2023-12-07 16:52:12 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# minecraft_server_updater
-minecraft server updater
+# Minecraft Server Launcher
+
+A Node.js script for managing and updating Minecraft servers.
+
+## Introduction
+
+This script automates the management of Minecraft server instances. It facilitates the installation, update, and management of multiple server versions, ensuring you're always up-to-date with the latest release from `piston-meta.mojang.com`.
+
+## Features
+
+- **Automatic Updates**: Checks and installs the latest Minecraft server version.
+- **Multiple Server Management**: Manage and switch between different server versions.
+- **Configuration Management**: Maintain and port server configurations and worlds across versions.
+
+## Installation
+
+[Include detailed steps on how to install this script, along with any prerequisites needed.]
+
+## Usage
+
+- **View Launcher Version**
+
+  ```sh
+  ./minecraft_server_launcher [-v | --version]
+  ```
+
+- **Launch the lastest installed mincreaft server**
+  ```sh
+  ./minecraft_server_launcher
+  ```
+- **Start a Specific Server Version**
+  ```sh
+  ./minecraft_server_launcher <version>
+  ```
+- **List Installed Servers**
+
+  ```sh
+  ./minecraft_server_launcher [-l | --list]
+  ```
+
+- **List Available Servers for download**
+
+  ```sh
+  ./minecraft_server_launcher [-la | --list-available]
+  ```
+
+- **Start a Specific Server Version**
+  ```sh
+  ./minecraft_server_launcher <version>
+  ```
+- **Install a Specific Server Version**
+  ```sh
+  ./minecraft_server_launcher [-i | --install] <version> <optional: version to copy worlds and configs from>
+  ```
+
+## Additional Information
+
+The script uses this endpoint to retrieve the latest game version:
+`https://piston-meta.mojang.com/mc/game/version_manifest_v2.json`
+
+## Contributing
+
+Contributions are welcome! Please feel free to submit pull requests, suggest features, or report issues. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+This project is licensed under the GNU General Public License v3.0 - see the [LICENSE](LICENSE) file for details.
---
+++ src/types/packages.ts
@@ -0,0 +1,162 @@
+export interface Packages {
+    arguments: Arguments
+    assetIndex: AssetIndex
+    assets: string
+    complianceLevel: number
+    downloads: Downloads
+    id: string
+    javaVersion: JavaVersion
+    libraries: Library[]
+    logging: Logging
+    mainClass: string
+    minimumLauncherVersion: number
+    releaseTime: string
+    time: string
+    type: string
+}
+
+export interface Arguments {
+    game: any[]
+    jvm: [
+        Jvm,
+        Jvm2,
+        Jvm3,
+        string,
+        string,
+        string,
+        string,
+        string,
+        string,
+        string,
+        string
+    ]
+}
+
+export interface Jvm {
+    rules: Rule[]
+    value: string[]
+}
+
+export interface Rule {
+    action: string
+    os: Os
+}
+
+export interface Os {
+    name: string
+}
+
+export interface Jvm2 {
+    rules: Rule2[]
+    value: string
+}
+
+export interface Rule2 {
+    action: string
+    os: Os2
+}
+
+export interface Os2 {
+    name: string
+}
+
+export interface Jvm3 {
+    rules: Rule3[]
+    value: string
+}
+
+export interface Rule3 {
+    action: string
+    os: Os3
+}
+
+export interface Os3 {
+    arch: string
+}
+
+export interface AssetIndex {
+    id: string
+    sha1: string
+    size: number
+    totalSize: number
+    url: string
+}
+
+export interface Downloads {
+    client: Client
+    client_mappings: ClientMappings
+    server: Server
+    server_mappings: ServerMappings
+}
+
+export interface Client {
+    sha1: string
+    size: number
+    url: string
+}
+
+export interface ClientMappings {
+    sha1: string
+    size: number
+    url: string
+}
+
+export interface Server {
+    sha1: string
+    size: number
+    url: string
+}
+
+export interface ServerMappings {
+    sha1: string
+    size: number
+    url: string
+}
+
+export interface JavaVersion {
+    component: string
+    majorVersion: number
+}
+
+export interface Library {
+    downloads: Downloads2
+    name: string
+    rules?: Rule4[]
+}
+
+export interface Downloads2 {
+    artifact: Artifact
+}
+
+export interface Artifact {
+    path: string
+    sha1: string
+    size: number
+    url: string
+}
+
+export interface Rule4 {
+    action: string
+    os: Os4
+}
+
+export interface Os4 {
+    name: string
+}
+
+export interface Logging {
+    client: Client2
+}
+
+export interface Client2 {
+    argument: string
+    file: File
+    type: string
+}
+
+export interface File {
+    id: string
+    sha1: string
+    size: number
+    url: string
+}
